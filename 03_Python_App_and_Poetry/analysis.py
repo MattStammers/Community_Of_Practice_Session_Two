ff4ea9c9b94e9144775404c0d882732674d9bbbd
Matt Stammers <matthew.stammers@uhs.nhs.uk>
2024-03-22 22:16:58 +0000

    working app but without tests

--- 03_Python_App_and_Poetry/analysis.py
+++ 03_Python_App_and_Poetry/analysis.py
@@ -1,48 +1,17 @@
-# Import pypi imports
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-
-# Import local imports
 from app.decision_tree import Decision_tree
 from app.logistic_reg import Logictic_model
 from app.pre_process import Pre_process
 from app.random_forest import Random_forest
-from sklearn.metrics import accuracy_score, roc_auc_score, roc_curve
-from sklearn.preprocessing import StandardScaler
 
-# Load data
 df = pd.read_csv("app/data/KaggleV2-May-2016.csv")
-df.head()
 
-# Preprocess data
 pre_processer = Pre_process(df)
 df_proc = pre_processer.format_and_get_df()
-# print(df_proc)
+print(df_proc)
 
-# Scale the processed data
-scaler = StandardScaler()
-df_proc_scaled = scaler.fit_transform(df_proc)
-# print(df_proc_scaled)
-
-df_proc_scaled_df = pd.DataFrame(df_proc_scaled, columns=df_proc.columns)
-print(df_proc_scaled_df)
-# Assuming 'df' is your DataFrame after preprocessing
-df_proc_scaled_df = df_proc_scaled_df.dropna(
-    subset=["No-show"]
-)  # Drops rows where 'No-show' is NaN
-df_proc_scaled_df["No-show"] = df_proc_scaled_df["No-show"].map({"Yes": 1, "No": 0})
-# Example check for NaN values in 'y' before training
-print(df_proc_scaled_df["No-show"].head())
-if df_proc_scaled_df["No-show"].isnull().any():
-    raise ValueError("NaN values found in target variable 'No-show'")
-
-
-# Logistic Regression Model
-# Assuming Logictic_model can accept a max_iter parameter
-log = Logictic_model(df_proc_scaled_df)  # Adjusted with scaled data
+log = Logictic_model(df_proc)
 log_reg = log.train_model()
-
 feature_importance = abs(log_reg.coef_[0])
 log.feature_importance(
     feature_importance,
@@ -53,20 +22,16 @@ log.calc_roc(
     log_reg, "logistic_regression", "ROC_curve_for_logistic_regression_classifier"
 )
 
-# Decision Tree Model
-dt = Decision_tree(df_proc_scaled_df)  # Adjusted with scaled data
+dt = Decision_tree(df_proc)
 dt_model = dt.train_model()
-
 feature_importances = dt_model.feature_importances_
 dt.feature_importance(
     feature_importances, "decision_tree", "feature_importance_decision_tree_classifier"
 )
 dt.calc_roc(dt_model, "decision_tree", "ROC_curve_for_decision_tree_classifier")
 
-# Random Forest Model
-rf = Random_forest(df_proc_scaled_df)  # Adjusted with scaled data
+rf = Random_forest(df_proc)
 rf_model = rf.train_model()
-
 feature_importances = rf_model.feature_importances_
 rf.feature_importance(
     feature_importances, "random_forest", "feature_importance_random_forest_classifier"
--- 03_Python_App_and_Poetry/app/logistic_reg.py
+++ 03_Python_App_and_Poetry/app/logistic_reg.py
@@ -13,6 +13,6 @@ class Logictic_model(Basic_model):
 
     def train_model(self):
         self.train_data()
-        lr_model = LogisticRegression(max_iter=1000)
+        lr_model = LogisticRegression(max_iter=10000)
         lr_model.fit(self.X_train, self.y_train)
         return lr_model
